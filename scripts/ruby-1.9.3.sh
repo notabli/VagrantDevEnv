6611a753bf21687dcd1ec2fc1a81714a99395e3d
Jordan Morano <jordan.morano@gmail.com>
2014-07-11 10:32:08 -0400

    Create ruby-1.9.3.sh

---
+++ scripts/ruby-1.9.3.sh
@@ -0,0 +1,23 @@
+# Installs RVM & Ruby 1.9.3 on 12.04 LTS Ubuntu
+# add to Vagrantfile:
+#  config.vm.provision 'shell', path: 'ruby-1.9.3.sh', privileged: false, keep_color: true
+
+if [[ -s "/home/vagrant/.rvm/scripts/rvm" ]] ; then
+  echo 'RVM installed, skipping RVM install'
+else
+  curl -sSL https://get.rvm.io | bash -s stable
+fi
+
+source '/home/vagrant/.rvm/scripts/rvm'
+
+if rvm list strings | grep -lq ruby-1.9.3 ; then
+  echo 'Ruby 1.9.3 installed. Skipping installed.'
+else
+  rvm autolibs packages
+  rvm requirements
+  rvm mount https://rvm.io/binaries/ubuntu/12.04/x86_64/ruby-1.9.3-p547.tar.bz2
+  rvm use 1.9.3-p547 --default
+  gem update bundler
+fi
+
+echo 'Setting Ruby 1.9.3 as default'
