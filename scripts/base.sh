4109a877e9b4bb4fb7d08b0a9c8ae16d0f846d54
Patrick Berkeley <patrickberkeley@gmail.com>
2014-09-23 20:02:27 -0400

    Adds a line to bash profile to change into /vagrant by default.

--- scripts/base.sh
+++ scripts/base.sh
@@ -3,3 +3,7 @@ apt-get update -y
 # Common Packages
 # memcached and redis will be running on their standard ports.
 apt-get install -y curl git redis-server memcached libcurl3 libcurl3-gnutls libcurl4-openssl-dev
+
+if ! grep -lq  'cd /vagrant' /home/vagrant/.bash_profile ; then
+  echo "cd /vagrant" >> /home/vagrant/.bash_profile
+fi
